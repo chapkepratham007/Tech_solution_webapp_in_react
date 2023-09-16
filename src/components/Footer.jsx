49898ff7d17b23f10cc341322334bb019d3e382c
pratham <chapkepratham@gmail.com>
2023-09-16 21:13:08 +0530

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../styles/footer.scss"
+const Footer = () => {
+  return (
+   <footer>
+    <div>
+    <h1>TechStar</h1>
+    <p>@all right reseved</p>
+    </div>
+
+    <div>
+        <h5>follow us</h5>
+        <div>
+            <a href="https://www.instagram.com">instagram</a>
+            <a href="https://ww.linkedin.com">LinkedIn</a>
+            <a href="https://www.github.com">Github</a>
+        </div>
+    </div>
+   </footer>
+  )
+}
+
+export default Footer
